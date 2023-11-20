d2c907addc8038c5b933b8e246cb227eb35bba30
yanrui <2730308008@qq.com>
2023-11-20 22:08:07 +0800

    增加prompt富集功能

---
+++ prompt多样性富集/基于向量相似度去重.py
@@ -0,0 +1,50 @@
+import pickle
+import faiss
+import numpy as np
+import json
+
+DATA_FILE = "100k_sft_embeddings.pkl"
+
+if __name__ == "__main__":
+    kkk = 0
+    embeddings, prompt_list, input_json_data = None, None, None
+    checkpoint = pickle.load(open(DATA_FILE,"rb"))
+    for key in checkpoint:
+        locals()[key] = checkpoint[key]
+
+
+    while 1:
+        pickle.dump({
+                "embeddings": embeddings,
+                "prompt_list": prompt_list,
+                "input_json_data":input_json_data,
+            },
+            open(f"checkpoint.pkl","wb"))
+        
+        print(f"Round{kkk} 开始过滤～")
+        quantizer = faiss.IndexFlatL2(768)  
+        index = faiss.IndexIVFFlat(quantizer, 768, 1000, faiss.METRIC_INNER_PRODUCT)
+        index.train(embeddings)
+        index.add(embeddings)                
+        index.nprobe = 10   
+
+        del_list = []           
+        D, I = index.search(embeddings, 2)
+        for (i, j), s in zip(I[:,:2],D[:,1]):
+            #print(prompt_list[i],prompt_list[j],s,"\n\n")
+            if j > i and s >0.9:
+                del_list.append(i)
+
+        if del_list == []:
+            break
+
+        for idx in sorted(del_list, reverse=True):
+            embeddings = np.delete(embeddings, idx, axis=0)
+            del prompt_list[idx]
+            del input_json_data[idx]
+
+        print(f"Round{kkk} 过滤去除{len(del_list)}个prompt～\n")
+        kkk +=1 
+
+        
+    json.dump(open("yanrui_去重结果.json","w") , "".join(input_json_data), ensure_ascii=False)
---
+++ prompt多样性富集/获取文本嵌入.py
@@ -0,0 +1,55 @@
+import json
+import numpy as np
+import datasets
+import faiss     
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import cos_sim
+import pickle
+
+
+def get_prompt(data):
+    prompt = ""
+    for x in data["conversations"][:-1]:
+        prompt += x['from'].capitalize() +":\n" + x["value"] + "\n"
+        break
+    return prompt
+
+
+if __name__ =="__main__":
+
+    # DATA_FILE = "train_3.5M_CN.json"
+    DATA_FILE = "10w.json"
+
+    prompt_list = []
+    input_json_data = open(DATA_FILE).readlines()
+    for line in input_json_data:
+        data = json.loads(line)
+        prompt = get_prompt(data)
+        prompt_list.append(prompt)
+
+
+    # 获取embeddings
+    model = SentenceTransformer('bge-base-zh').cuda()
+    embeddings = model.encode(prompt_list, normalize_embeddings=True,batch_size=1024,show_progress_bar=True)
+
+    # 开启GPU加速
+    # pool = model.start_multi_process_pool()
+    # embeddings = model.encode_multi_process(prompt_list, pool,batch_size=1024)
+    import gc
+    import torch
+    del model 
+    torch.cuda.empty_cache()
+    gc.collect() 
+    torch.cuda.empty_cache()
+    
+
+    pickle.dump({
+                "embeddings": embeddings,
+                "prompt_list": prompt_list,
+                "input_json_data":input_json_data,
+            },
+            open(f"{len(embeddings)//1000}k_sft_embeddings.pkl","wb"))
+    
+
+
+        

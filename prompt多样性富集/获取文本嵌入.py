043ce02e27f7b761f404130b3307d078c70e8f74
yanrui <2730308008@qq.com>
2023-11-21 00:32:57 +0800

    添加进度条代码

--- prompt多样性富集/获取文本嵌入.py
+++ prompt多样性富集/获取文本嵌入.py
@@ -5,7 +5,7 @@ import faiss
 from sentence_transformers import SentenceTransformer
 from sentence_transformers.util import cos_sim
 import pickle
-
+import tqdm
 
 def get_prompt(data):
     prompt = ""
@@ -15,25 +15,41 @@ def get_prompt(data):
     return prompt
 
 DATA_FILE = "train_3.5M_CN.json"
+# DATA_FILE = "100k.json"
 SAVE_FILE = "k_sft_embeddings.pkl"
 
 if __name__ =="__main__":
 
     prompt_list = []
     input_json_data = open(DATA_FILE).readlines()
-    for line in input_json_data:
-        data = json.loads(line)
-        prompt = get_prompt(data)
-        prompt_list.append(prompt)
+    prompt_list = [get_prompt(json.loads(line)) for line in input_json_data]
+    
 
-
-    # 获取embeddings
+    # 获取embedding_model
     model = SentenceTransformer('bge-base-zh').cuda()
 
     # embeddings = model.encode(prompt_list, normalize_embeddings=True,batch_size=1024,show_progress_bar=True)
     # 开启GPU加速
     pool = model.start_multi_process_pool()
-    embeddings = model.encode_multi_process(prompt_list, pool,batch_size=1024)
+
+    # Define the batch size
+    batch_size = 1024 * len(pool['processes']) * 4
+
+
+    # Calculate the number of batches
+    num_batches = len(prompt_list) // batch_size + (len(prompt_list) % batch_size != 0)
+
+    # Create an empty list to store the embeddings
+    embeddings = []
+    # Process each batch
+    for i in tqdm.tqdm(range(num_batches)):
+        start_index = i * batch_size
+        end_index = min((i + 1) * batch_size, len(prompt_list))
+        batch = prompt_list[start_index:end_index]
+        batch_embeddings = model.encode_multi_process(batch, pool, batch_size=1024)
+        embeddings.append(batch_embeddings)
+
+    embeddings = np.concatenate(embeddings)
     import gc
     import torch
     del model 
